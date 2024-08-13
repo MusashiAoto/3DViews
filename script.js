cb0234378c32ff4f8d56661c36155793b482498d
MusashiAoto <43405776+MusashiAoto@users.noreply.github.com>
2024-08-13 12:16:40 +0900

    FirstCommit

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('model1').href = 'https://scaniverse.com/scan/hmuc3pylby37nt7f';
+document.getElementById('model2').href = 'https://scaniverse.com/scan/ebcq5xcj7vrrabjm';
+document.getElementById('model3').href = 'https://scaniverse.com/scan/ynvhj4lpe5llmo7b';
